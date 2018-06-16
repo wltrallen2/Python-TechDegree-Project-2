2d39ca352e4c17165838c7e89a5a740eaea70879
Walter Allen <wltrallen2@gmail.com>
2018-06-15 22:18:06 -0500

    1st Effort for Padding
    - Padding contain contained only within the messages.py script.

--- messages.py
+++ messages.py
@@ -12,6 +12,8 @@ When encrypting, the user can also select the following options:
 *********************************************************************
 '''
 import os
+import random
+import string
 import textwrap
 from atbash import Atbash
 from caesar import Caesar
@@ -42,8 +44,8 @@ INVALID_ACTION_MESSAGE = '''
 You've entered an invalid action.
 Please enter [E]ncrypt, [D]ecrypt, or [Q]uit.'''
 
-CIPHER_PROMPT_PART_1 = "Please choose the cipher you would like to use to {} \
-your message:"
+CIPHER_PROMPT_PART_1 = '''
+Please choose the cipher you would like to use to {} your message:'''
 
 CIPHER_PROMPT_PART_2 = "Enter the number of your choice here ==> "
 
@@ -54,11 +56,32 @@ CIPHER_PROMPT_ERROR_MESSAGE = \
 
 MESSAGE_PROMPT = "Please enter the message you'd like to {}: \n"
 
+DOES_USER_WANT_TO_PAD = \
+"\nDo you want to use a one-time pad to {} your message? [Y/N] "
+
+IS_TO_BE_RANDOM = \
+"Do you want your pad to be generated randomly or would you like to \
+\nenter the pad manually? [R]andom/[M]anually ==> "
+
+ENTER_PADDED_MESSAGE = \
+"\nPlease enter the pad for your message:\n"
+
+ENTERED_INVALID_RESPONSE = \
+"*** You've entered an invalid response. Please try again. ***\n"
+
+PAD_REMINDER = \
+'''*** Don't forget your pad. You will need it to decrypt the message. ***
+Your pad is {}.'''
+
 KEYWORD_PROMPT = \
 "\nPlease enter the key word you'd like to use for this cipher ==> "
 
 KEY_NUMBER_PROMPT = \
 "\nPlease enter the key number you'd like to use for this cipher ==> "
+
+INVALID_PAD = '''
+*** The number of characters in your pad must match the number of characters
+in your message. ***'''
 
 OUTPUT_PREMESSAGE = "\nHere is your {}ed message using the {} cipher: "
 
@@ -116,6 +139,53 @@ def prompt_for_message(action):
     """
     return input(MESSAGE_PROMPT.format(action))
 
+def prompt_for_padding(action, message):
+    while True:
+        is_to_be_padded = (input(DOES_USER_WANT_TO_PAD.format(action))).upper()
+        if is_to_be_padded in ['YES', 'Y']:
+            is_to_be_random = input(IS_TO_BE_RANDOM).upper()
+            if is_to_be_random in ['RANDOM', 'R']:
+                padded_message = get_padded_message(message)
+            elif is_to_be_random in ['MANUALLY', 'M']:
+                pad = input(ENTER_PADDED_MESSAGE).upper()
+                if len(pad) < len(message):
+                    print(INVALID_PAD)
+                    continue
+                padded_message = get_padded_message(message, pad)
+            else:
+                print(INVALID_ACTION_MESSAGE)
+                continue
+        elif is_to_be_padded in ['NO', 'N']:
+            padded_message = message
+        else:
+            print(ENTERED_INVALID_RESPONSE)
+            continue
+
+        print('PADDED MESSAGE: {}'.format(padded_message))
+        return padded_message
+
+def get_padded_message(message, pad = None):
+    #TODO: Fix padded_message
+    alphabet = string.ascii_uppercase
+    if not pad:
+        pad = ''.join(random.choice(alphabet) for i in range(len(message)))
+
+    print(PAD_REMINDER.format(pad))
+    padded_message = ''
+    # import pdb; pdb.set_trace()
+    for index in range(len(message)):
+        letter = message[index]
+        if letter.isalpha():
+            shift = alphabet.index(pad[index])
+            padded_message += shift_cap_letter(letter.upper(), shift)
+    return padded_message
+
+def shift_cap_letter(letter, shift):
+    shifted_value = ord(letter) + shift
+    if shifted_value > ord('Z'):
+        shifted_value -= 26
+    return chr(shifted_value)
+
 def create_cipher_instance(cipher):
     """create_cipher_instance(cipher) returns a cipher instance
     of the correct type after propmtping for any necessary key
@@ -169,6 +239,7 @@ if __name__ == "__main__":
         clear_screen()
         cipher = prompt_for_cipher(action)
         message = prompt_for_message(action)
+        message = prompt_for_padding(action, message)
         cipher_instance = create_cipher_instance(cipher)
         coded_message = perform_action(action, cipher_instance, message)
         output_results(action, cipher, coded_message)
