1c6b408cb64b40791bb174edb82dc26c05cb3afe
Walter Allen <wltrallen2@gmail.com>
2018-07-05 16:55:58 -0500

    Updated docstrings in keyword_cipher

--- keyword_cipher.py
+++ keyword_cipher.py
@@ -1,15 +1,3 @@
-"""keyword_cipher.py encodes and decodes messages using the Keyword Cipher,
-which creates a substitution cipher by removing each of the letters in a
-<keyword> (ignoring symbols, whitespace, and duplicate letters)
-from the English alphabet, then appending the <keyword> to the beginning
-of the alphabet. Next, a position-for-position substituion is made.
-
-If the character is not one of the 26 letters of the alphabet,
-it passes through the coding untouched. In other words, numbers and
-symbols will appear in the coded or decoded messages as the same
-numbers and symbols.
-"""
-
 from ciphers import Cipher
 
 
@@ -17,19 +5,39 @@ ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
 class Keyword(Cipher):
+    """This class encodes and decodes messages using the Keyword Cipher,
+    which creates a substitution cipher by removing each of the letters in a
+    <keyword> (ignoring symbols, whitespace, and duplicate letters)
+    from the English alphabet, then appending the <keyword> to the beginning
+    of the alphabet. Next, a position-for-position substituion is made.
+
+    If the character is not one of the 26 letters of the alphabet,
+    it passes through the coding untouched. In other words, numbers and
+    symbols will appear in the coded or decoded messages as the same
+    numbers and symbols.
+    """
+
     def __init__(self, key_word = ''):
         """The initializer accepts a keyword <key_word> and sets then
-        instance variable <keyword_alphabet>.
+        instance variable <keyword_alphabet>. It also creates the
+        <arguments_dict> variable which identifies that this class needs a
+        'Key Word' argument that is passed in as a str object.
         """
         self.arguments_dict = {'Key Word': str}
         self.keyword_alphabet = self.__create_keyword_alphabet(key_word.upper())
 
     def set_arguments(self, new_args_dict):
+        """This method sets the <keyword_alphabet> variable using the value
+        that has been passed using the key 'Key Word' in the <arguments_dict>.
+        The value should be a str object. The <keyword_alphabet> variable
+        is the shifted alphabet that is used in the main substition cipher
+        to both encrypt and decrypt messages.
+        """
         self.keyword_alphabet = \
             self.__create_keyword_alphabet(new_args_dict['Key Word'].upper())
 
     def encrypt(self, message):
-        """encrypt(message) encrypts the <message> by substituting the letters
+        """Returns the ecrypted message by substituting the letters
         in the standard alphabet with the letters in the class variable
         <keyword_alphabet>.
         """
@@ -38,7 +46,7 @@ class Keyword(Cipher):
                                          self.keyword_alphabet)
 
     def decrypt(self, message):
-        """decrypt(message) decrypts the <message> by substituting the letters
+        """Returns the decrypted message by substituting the letters
         in the class variable <keyword_alphabet> with the letters in the
         standard alphabet.
         """
@@ -48,11 +56,11 @@ class Keyword(Cipher):
 
 
     def __create_keyword_alphabet(self, key_word):
-        """__create_keyword_alphabet(self, key_word) returns a keyword_alphabet by
-        removing the letters in the <key_word> from the English alphabet (ignoring
-        symbols, whitespace, and duplicate letters), then appending the letters
-        to the beginning of the remaining alphabet letters (again igoring symbols,
-        whitespace, and duplicate letters).
+        """Returns a keyword_alphabet by removing the letters in the <key_word>
+        from the English alphabet (ignoring symbols, whitespace, and duplicate
+        letters), then appending the letters to the beginning of the remaining
+        alphabet letters (again igoring symbols, whitespace, and duplicate
+        letters).
         """
         keyword_push = ''
         keyword_alphabet = ALPHABET
@@ -64,9 +72,9 @@ class Keyword(Cipher):
         return keyword_alphabet
 
     def __substitute_letters(self, message, from_string, to_string):
-        """__substitute_letters(message, from_string, to_string) returns a string
-        in which each letter is located in the from_string and replaced with
-        the letter in the same index position in the to_string.
+        """Returns a string in which each letter is located in the
+        from_string and replaced with the letter in the same index position
+        in the to_string.
         """
         coded_message = ''
         for letter in message.upper():
