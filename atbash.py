7e087693ec74d3bb846e0464408b2388949a11f2
Walter Allen <wltrallen2@gmail.com>
2018-07-05 16:45:41 -0500

    Updated docstrings in Atbash subclass

--- atbash.py
+++ atbash.py
@@ -1,22 +1,23 @@
-"""atbash.py encodes and decodes messages using the Atbash Cipher,
-which maps each letter of the alphabet to its reverse. For example,
-the 1st letter (A) is mapped to the last, or 26th letter, (Z), and
-so on.
-
-If the character is not one of the 26 letters of the alphabet,
-it passes through the coding untouched. In other words, numbers and
-symbols will appear in the coded or decoded messages as the same
-numbers and symbols.
-"""
 from ciphers import Cipher
 import string
 
 ALPHABET = string.ascii_letters.upper()
 
 class Atbash(Cipher):
+    """This class encodes and decodes messages using the Atbash Cipher,
+    which maps each letter of the alphabet to its reverse. For example,
+    the 1st letter (A) is mapped to the last, or 26th letter, (Z), and
+    so on.
+
+    If the character is not one of the 26 letters of the alphabet,
+    it passes through the coding untouched. In other words, numbers and
+    symbols will appear in the coded or decoded messages as the same
+    numbers and symbols.
+    """
+    
     def encrypt(self, message):
-        """encrypt(self, message) encrypts the <message> using
-        the Atbash Cipher and returns the encoded message.
+        """Returns an encoded message by encrypting the <message> using
+        the Atbash Cipher.
         """
         coded_message = ''
         for index in range(len(message)):
@@ -29,7 +30,7 @@ class Atbash(Cipher):
         return coded_message
 
     def decrypt(self, message):
-        """decrypt(self, message) decrypts the <message> using
-        the Atbash Cipher and returns the decoded message.
+        """Returns the decrypted message by decrypting the <message> using
+        the Atbash Cipher.
         """
         return self.encrypt(message)
